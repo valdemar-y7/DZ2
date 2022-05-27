f63895afa28e624c40bd6b53eb1e2ae0caac9676
Владимир Юдин <yudin_v_m@mail.ru>
2022-05-27 15:38:16 +0300

    Required

---
+++ functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void random(int*, int);
+void print(int*, int);
+void inv(int*, int);
+void notDown(int*, int);
+void notUp(int*, int);
+void (*fun(int*, int))(int*, int);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "functions.h"
+
+int main()
+{
+	srand(time(0));
+	int size = 0;
+	cin >> size;
+	int* mas = new int[size];
+	random(mas, size);
+	fun(mas, size)(mas, size);
+	print(mas, size);
+}
